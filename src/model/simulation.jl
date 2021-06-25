dbc8d79f8ee823f36eb87bd53515754b6201ecf0
erathorn <wahle.joh@gmail.com>
2021-06-25 07:01:13 +0000

    fixed nuts and removed memory problems

--- src/model/simulation.jl
+++ src/model/simulation.jl
@@ -268,15 +268,12 @@ function gradlogpdf!(m::Model, x::N, block::Integer=0, transform::Bool=false)::T
   #lp = logpdf(m, setdiff(params, targets), transform)
 
 
-  # use thread parallelism
   # likelihood
-  lik_res = @spawn gradlogpdf(m, targets)
+  v, grad =  gradlogpdf(m, targets)
 
   # prior
   vp, gradp = gradlogpdf(m[params[1]], x)
 
-  # get results from threads
-  v, grad = fetch(lik_res)
 
   vp+v, gradp.+grad[1]
 end
